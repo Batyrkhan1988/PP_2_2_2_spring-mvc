34a328c9e4e438707a11712a5686ff4535ff43e0
Verda05 <verda1988@gmail.com>
2022-04-05 13:22:01 +0300

    Амирханов Б.А.

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,21 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarService implements CarServiceImpl {
+
+    @Override
+    public List<Car> getCarsList(int count) {
+        List<Car> carsList = new ArrayList<>();
+        carsList.add(new Car("Priora", "2170", "red"));
+        carsList.add(new Car("Vesta", "2180", "yellow"));
+        carsList.add(new Car("Kalina", "1117", "white"));
+        carsList.add(new Car("Granta", "2190", "black"));
+        carsList.add(new Car("GAZ", "3102", "silver"));
+        return carsList.stream().limit(count).collect(Collectors.toList());
+    }
+}
